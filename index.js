1bf20ff401934f1ff02f2b6c5db3fd312a798322
Tate Larkin <tatelarkin@icloud.com>
2021-09-29 19:41:51 -0600

    heroku fix

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.get( '/auth/google/home',
 passport.use(new FacebookStrategy({
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: "https://a-reddit-clone.herokuapp.com//auth/facebook/home"
+  callbackURL: "https://a-reddit-clone.herokuapp.com/auth/facebook/home"
 },
 function(accessToken, refreshToken, profile, done) {
   User.findOrCreate({ facebookId: profile.id }, function(err, user) {
